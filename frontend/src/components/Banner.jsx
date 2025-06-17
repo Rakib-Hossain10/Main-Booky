1f71bd03e4a6f5d9676d50f1444a102f8465aa56
Rakib-Hossain10 <iamrakib1221@gmail.com>
2025-06-17 14:12:21 +0800

    Second Commit

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -25,10 +25,11 @@ function Banner() {
                 <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
                 <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
               </svg>
-              <input type="text" className="grow" placeholder="Email" />
+              <p className="text-pink-500 font-bold flex items-center justify-center">"Discover Your Next Favorite Book Today"</p>
+              {/* <input type="text" className="grow" placeholder="Email" /> */}
             </label>
           </div>
-          <button className="btn mt-6 mb-20 btn-secondary">Get Started</button>
+          {/* <button className="btn mt-6 mb-20 btn-secondary">Get Startede</button> */}
         </div>
         <div className=" order-1 w-full mt-20 md:w-1/2">
           <img
